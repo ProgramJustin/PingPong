12d64cb31884bf492ec7b96213a51566fdfe7f8d
Justin Gomez <programjustin@gmail.com>
2017-05-05 10:14:21 -0700

    push the users input into the array numberArray and output the result to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-var userNumber = "";
+var numberArray = [];
+
+console.log(numberArray[0]);
+
+var inputToArray = function(userInput) {
+
+if (numberArray[0] === undefined) {
+  numberArray.push(userInput);
+}
+
+};
+
+
+
+// var numberArray = [userNumber];
+// console.log(userNumber);
 
 
 $(document).ready(function() {
@@ -8,7 +23,11 @@ $(document).ready(function() {
 
     var userInput = parseInt($('input').val());
 
+    var pingPong = inputToArray(userInput);
+    alert("test");
+
     console.log(typeof userInput);
+    $("#result").text(numberArray[0]);
 
   });
 
